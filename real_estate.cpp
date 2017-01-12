238c4fbfce814192cafea39a5ff6c7de7de7f7c8
Nico Schottelius <nico@feder.schottelius.org>
2017-01-12 19:59:33 +0100

    Add satellites (wrong solution)

--- real_estate.cpp
+++ real_estate.cpp
@@ -4,21 +4,22 @@
 #include <boost/config.hpp>
 #include <iostream>
 #include <string>
-#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/read_dimacs.hpp>
+
 
 using namespace std;
 using namespace boost;
 
 typedef adjacency_list_traits<vecS, vecS, directedS> Traits;
 
-typedef pair<int, int> EdgeCapacity;
+// typedef pair<int, int> EdgeCapacity;
 
 typedef adjacency_list<vecS, vecS, directedS,
                        property<vertex_name_t, std::string>,
-                       property<edge_capacity_t, EdgeCapacity,
-                                property<edge_residual_capacity_t, EdgeCapacity,
+                       property<edge_capacity_t, int,
+                                property<edge_residual_capacity_t, int,
                                          property<edge_reverse_t, Traits::edge_descriptor> > > > Graph;
 
 
@@ -42,8 +43,6 @@ void my_add_edge(Graph &g, int from, int to, EdgeCapacity cap, Capacity &c, Resi
 
     rev[e] = e_rev;
     rev[e_rev] = e;
-
-
 }
 
 
@@ -71,9 +70,12 @@ int main()
         }
         vector<vector <int > > bid(n, vector<int>(m));
 
+        int highest_bid = 0;
         for(int i=0; i < n; ++i) {
             for(int j=0; j < m; ++j) {
                 cin >> bid[i][j];
+
+                if(bid[i][j] > highest_bid) highest_bid = bid[i][j];
             }
         }
 
@@ -102,9 +104,10 @@ int main()
         my_add_edge(g, source_vertex, target_vertex, make_pair(highest_possible_flow, 1), capacity, residual_capacity, rev);
 
         /******************** determine flow ********************/
-        EdgeCapacity res = push_relabel_max_flow(g, s, t);
+//        EdgeCapacity res = push_relabel_max_flow(g, s, t);
 
-        cout << res.second << " " << res.first << endl;
+//         successive_shortest_path_nonnegative_weights()
+//        cout << res.second << " " << res.first << endl;
 
     }
 
