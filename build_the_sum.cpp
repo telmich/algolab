f7883e36e7cb059e1317dfabca64603b13b4c94f
Nico Schottelius <nico@nico-notebook.schottelius.org>
2017-08-22 22:42:33 +0200

    init the course
    Signed-off-by: Nico Schottelius <nico@nico-notebook.schottelius.org>

---
+++ build_the_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int t;
+
+    cin >> t;
+
+    while(t--) {
+        int m;
+        cin >> m;
+
+        double sum = 0;
+        double input;
+
+        while(m--) {
+            cin >> input;
+
+            sum += input;
+        }
+        cout << sum << endl;
+
+    }
+
+}
