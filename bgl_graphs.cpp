f0353994b7055127a4b652c1989b88125ae0ad68
Nico Schottelius <nico@nico-notebook.schottelius.org>
2017-10-24 14:12:02 +0200

    Remove some uncessary stuff from bgl thing one

--- bgl_graphs.cpp
+++ bgl_graphs.cpp
@@ -61,35 +61,24 @@ int main()
 
         /* graph for finding node furthest away in MST */
         Graph g2(n);
-        property_map<Graph, edge_weight_t>::type weightmap2 = get(edge_weight, g);
-
-        vector<list<pair<int, int>>> mst_graph(n); /* pair: first = destination
-                                                      second = weight */
+        property_map<Graph, edge_weight_t>::type weightmap2 = get(edge_weight, g2);
 
         int total_weight = 0;
         for(int i=0; i < p.size(); i++) {
-            if(p[i] == i) {
-                cerr << "Skipping root: " << i << endl;
-
-                continue;
-            }
+            if(p[i] == i) { continue; } /* skip root */
 
             /* add edge in both (?) */
-
-
             Edge e, e2;
             bool sucess;
 
+            /* get weight from old graph */
             tie(e, sucess) = edge(p[i], i, g);
-
-            mst_graph[i].push_back(make_pair(p[i], weightmap[e]));
-            mst_graph[p[i]].push_back(make_pair(i, weightmap[e]));
-
             total_weight += weightmap[e];
-            cerr << i << "--" << p[i] << ": " << weightmap[e] << endl;
 
+            /* create new graph */
             tie(e2, sucess) = add_edge(p[i], i, g2);
             weightmap2[e2] = weightmap[e];
+
         }
 
         vector<int> d42(num_vertices(g2));
@@ -99,32 +88,9 @@ int main()
 
         sort(d42.begin(), d42.end());
 
-        int max_distance = ;
-        vector<int> distances(n,0);
+        int max_distance = *(d42.rbegin()) ;
 
-
-
-        // for(auto &ai: mst_graph[0]) {
-        //     cout << ai.first << " " << ai.second << endl;
-
-        // }
-
-
-        /* use distance_recorder */
-        // graph_traits<Graph>::vertices_size_type d[p.size()];
-
-        // Graph g2_copy(n);
-        // vector<Vertex> p2(boost::num_vertices(g2));
-
-        //
-        // breadth_first_search(g2, 0,
-        //                      boost::make_bfs_visitor(
-        //                          make_pair(boost::record_distances(d, boost::on_tree_edge()),
-        //                                    make_pair(boost::record_predecessors(p2.begin(), boost::on_tree_edge()),
-        //                                              copy_graph(g2_copy, boost::on_examine_edge())))) );
-
-
-
+        cout << total_weight << " " << max_distance << endl;
     }
 
 }
