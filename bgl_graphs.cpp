7d1c102aca51cc0b0f871277f73ee75dce182607
Nico Schottelius <nico@nico-notebook.schottelius.org>
2017-10-24 14:26:27 +0200

    Do find longest node in original tree, not in MST

--- bgl_graphs.cpp
+++ bgl_graphs.cpp
@@ -21,19 +21,9 @@ typedef adjacency_list   <vecS,
 
 typedef graph_traits<Graph>::edge_descriptor Edge;
 
-// all crap
-// typedef Graph::vertex_descriptor Vertex;
-
-// template <class NewGraph, class Tag>
-// inline graph_copier<NewGraph, Tag>
-// copy_graph(NewGraph& g, Tag) {
-//   return graph_copier<NewGraph, Tag>(g);
-// }
-
 
 int main()
 {
-
     int t;
     cin >> t;
 
@@ -56,14 +46,11 @@ int main()
             weightmap[e] = weight;
         }
 
-        vector <graph_traits <Graph>::vertex_descriptor>  p(num_vertices(g));
+        vector <graph_traits <Graph>::vertex_descriptor> p(num_vertices(g));
         prim_minimum_spanning_tree(g, &p[0]);
 
-        /* graph for finding node furthest away in MST */
-        Graph g2(n);
-        property_map<Graph, edge_weight_t>::type weightmap2 = get(edge_weight, g2);
-
         int total_weight = 0;
+
         for(int i=0; i < p.size(); i++) {
             if(p[i] == i) { continue; } /* skip root */
 
@@ -74,21 +61,16 @@ int main()
             /* get weight from old graph */
             tie(e, sucess) = edge(p[i], i, g);
             total_weight += weightmap[e];
-
-            /* create new graph */
-            tie(e2, sucess) = add_edge(p[i], i, g2);
-            weightmap2[e2] = weightmap[e];
-
         }
 
-        vector<int> d42(num_vertices(g2));
-        dijkstra_shortest_paths(g2, 0,
+        vector<int> d42(num_vertices(g));
+        dijkstra_shortest_paths(g, 0,
                                 distance_map(make_iterator_property_map(d42.begin(),
                                                                         get(vertex_index, g))));
 
         sort(d42.begin(), d42.end());
 
-        int max_distance = *(d42.rbegin()) ;
+        int max_distance = *(d42.rbegin());
 
         cout << total_weight << " " << max_distance << endl;
     }
