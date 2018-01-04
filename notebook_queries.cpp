16746be98d2ead0a879db95c13650318bbdcfb61
Nico Schottelius <nico@nico-notebook.schottelius.org>
2018-01-04 12:25:16 +0100

    Remove old tries

--- notebook_queries.cpp
+++ notebook_queries.cpp
@@ -3,92 +3,6 @@
 
 using namespace std;
 
-
-// int get_result(vector<int> &query,
-//                vector<vector<int>> &record,
-//                vector<int> &result)
-// {
-//     int k = query.size();
-//     int res = -1;
-
-//     int nextpos = query[0];
-
-//     for(int i=1; i < k; i++) {
-//         int question = query[i];
-//         int records_here = record[i][nextpos].size();
-
-//         if(records_here == 0) { /* no results listed */
-//             nextpos = -1;
-//             break;
-//         }
-
-//         for(int j=0; j < records_here; j++) {
-//             if(record[i][question].first == next) {
-//                 nextpos = record[i][question].second;
-//                 break;
-//             }
-//         }
-//         if(nextpos == -1) break;
-
-//         if(i == k) {
-//             res = result[nextpos];
-//         }
-
-//     }
-
-//     return res;
-// }
-
-
-// int store_element(int &value,
-//                   vector<int> &query,
-//                   vector<vector<pair<int,int>>> &record,
-//                   vector<int> &result)
-// {
-//     int k = query.size();
-
-//     int pos;
-//     int val = value;
-
-//     cerr << "Insert: ";
-
-//     result.push_back(value);
-//     pos = result.size()-1;
-
-//     cerr << " p/v: " << pos << " " << val;
-
-//     for(int i=k-1; i >=0; i--) {
-//         int val = query[i];
-//         cerr << " p/v: " << pos << " " << val;
-
-//         pair<int, int> foo = make_pair(val, pos);
-//         record[i].push_back(foo);
-//         pos = record[i].size()-1;
-//     }
-//     cerr << endl;
-// }
-
-
-// int get_element(vector<vector<int>>> records,
-//                 vector<int> query)
-// {
-//     vector<int> results;
-
-//     array1[3].push_back();
-
-//     /* start reverse, insert result at the end */
-
-//     array[k-1]
-
-
-//     results.push_back(res);
-//     entrynum = result.size()-1;
-
-
-// }
-
-
-
 void store_element(int &value,
                    vector<int> &pos,
                    vector<vector<int>> &table,
@@ -111,18 +25,18 @@ void store_element(int &value,
 
             table[t_index][t_pos] = next_table;
 
-            cerr << "Generating next table for " << i << "," << t_pos << ": " << next_table << " size: " << ci[i+1] << endl;
+            // cerr << "Generating next table for " << i << "," << t_pos << ": " << next_table << " size: " << ci[i+1] << endl;
 
         } else {
             next_table = table[t_index][t_pos];
         }
-        cerr << "Using table for " << i << "," << t_pos << ": " << next_table << endl;
+        // cerr << "Using table for " << i << "," << t_pos << ": " << next_table << endl;
 
         t_index = next_table;
         t_pos   = pos[i+1] - ai[i+1];
     }
 
-    cerr << "Storing " << value << " at " << t_index << "," << t_pos << endl;
+    // cerr << "Storing " << value << " at " << t_index << "," << t_pos << endl;
 
     table[t_index][t_pos] = value;
 
@@ -205,19 +119,19 @@ int main()
             vector<int> pos(k);
             int res;
 
-            cerr << "Looking for " ;
+            // cerr << "Looking for " ;
 
             for(int j=0; j < k; j++) {
 
                 cin >> pos[j];
-                cerr << pos[j] << " ";
+                // cerr << pos[j] << " ";
             }
 
 
-            cerr << endl;
+            // cerr << endl;
             cout << get_element(pos, table, ai) << endl;
         }
-        cerr << "----" << endl;
+        // cerr << "----" << endl;
 
 
     }
