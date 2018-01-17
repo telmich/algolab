ba09d29b71ccdbb227746f52f5350a3c66e33aed
Nico Schottelius <nico@nico-notebook.schottelius.org>
2018-01-17 22:56:12 +0100

    Add WA 50 version of surveilance

---
+++ surveilance_photographs.cpp
@@ -0,0 +1,138 @@
+// ALGOLAB BGL Tutorial 2
+// Flow example demonstrating
+// - interior graph properties for flow algorithms
+// - custom edge adder
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 flows.cpp -o flows ./flows
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 flows.cpp -o flows; ./flows
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/edmonds_karp_max_flow.hpp>
+// Namespaces
+using namespace std;
+// using namespace boost;
+
+#include <vector>
+
+// BGL Graph definitions
+// =====================
+// Graph Type with nested interior edge properties for Flow Algorithms
+typedef	boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+                              boost::property<boost::edge_capacity_t, long,
+                                              boost::property<boost::edge_residual_capacity_t, long,
+                                                              boost::property<boost::edge_reverse_t, Traits::edge_descriptor> > > >	Graph;
+// Interior Property Maps
+typedef	boost::property_map<Graph, boost::edge_capacity_t>::type		EdgeCapacityMap;
+typedef	boost::property_map<Graph, boost::edge_residual_capacity_t>::type	ResidualCapacityMap;
+typedef	boost::property_map<Graph, boost::edge_reverse_t>::type		ReverseEdgeMap;
+typedef	boost::graph_traits<Graph>::vertex_descriptor			Vertex;
+typedef	boost::graph_traits<Graph>::edge_descriptor			Edge;
+typedef	boost::graph_traits<Graph>::edge_iterator			EdgeIt;
+
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map and reverse edge map
+// ===================================================
+class EdgeAdder {
+	Graph &G;
+	EdgeCapacityMap	&capacitymap;
+	ReverseEdgeMap	&revedgemap;
+
+public:
+	// to initialize the Object
+	EdgeAdder(Graph & G, EdgeCapacityMap &capacitymap, ReverseEdgeMap &revedgemap):
+		G(G), capacitymap(capacitymap), revedgemap(revedgemap){}
+
+	// to use the Function (add an edge)
+	void addEdge(int from, int to, long capacity) {
+		Edge e, rev_e;
+		bool success;
+		boost::tie(e, success) = boost::add_edge(from, to, G);
+		boost::tie(rev_e, success) = boost::add_edge(to, from, G);
+		capacitymap[e] = capacity;
+		capacitymap[rev_e] = 0; // reverse edge has no capacity!
+		revedgemap[e] = rev_e;
+		revedgemap[rev_e] = e;
+	}
+};
+
+
+// Functions
+// =========
+// Function for an individual testcase
+void testcases() {
+	// Create Graph and Maps
+
+    int n, m, k, l;
+
+    cin >> n >> m >> k >> l;
+
+    vector<int> station(n,0);
+    vector<int> photo(n,0);
+
+    int tmp;
+
+    for(int i=0; i< k; i++) {
+        cin >> tmp;
+        station[tmp]++;
+    }
+
+    for(int i=0; i< l; i++) {
+        cin >> tmp;
+        photo[tmp]++;
+    }
+
+    int num_vertices = n + k + l + 2; /* original + photos + stations + start&end */
+    int s_vertex = n;
+    int t_vertex = n+1;
+
+
+	Graph G(num_vertices);
+	EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+	ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+	ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
+	EdgeAdder eaG(G, capacitymap, revedgemap);
+
+
+    for(int i=0; i< m; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        eaG.addEdge(a, b, 1); // from, to, capacity
+    }
+
+
+    for(int i=0; i< n; i++) {
+        if(station[i] > 0) {
+            eaG.addEdge(i, t_vertex, station[i]); // from, to, capacity
+        }
+        if(photo[i] > 0) {
+            eaG.addEdge(s_vertex, i, photo[i]); // from, to, capacity
+        }
+    }
+
+
+	// long flow1 = boost::push_relabel_max_flow(G, source, target);
+	long flow2 = boost::edmonds_karp_max_flow(G, s_vertex, t_vertex);
+
+    cout << flow2 << endl;
+
+}
+
+// Main function to loop over the testcases
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	int T;	cin >> T;
+	for (; T > 0; --T)	testcases();
+	return 0;
+}
