c66cb8b2ac039be15b868f998ee2c535fc172f41
Nico Schottelius <nico@nico-notebook.schottelius.org>
2018-01-29 14:20:44 +0100

    -- cerror

--- kingdomdefence.cpp
+++ kingdomdefence.cpp
@@ -10,8 +10,8 @@ using namespace std;
 using namespace boost;
 
 typedef adjacency_list_traits < vecS, vecS, directedS > Traits;
-typedef adjacency_list < listS, vecS, directedS,
-                         property < vertex_name_t, std::string >,
+typedef adjacency_list < vecS, vecS, directedS,
+                         no_property,
                          property < edge_capacity_t, long,
                                     property < edge_residual_capacity_t, long,
                                                property < edge_reverse_t, Traits::edge_descriptor > > > > Graph;
@@ -44,8 +44,6 @@ public:
 		tie(e, success) = add_edge(from, to, G);
 		tie(reverseE, success) = add_edge(to, from, G);
 
-        cerr << from << "->" << to << " " << capacity << endl;
-
 		capacitymap[e] = capacity;
 		capacitymap[reverseE] = 0;
 
@@ -66,72 +64,41 @@ int main()
 
         cin >> l >> p;
 
-        vector<int> gi(l);
-        vector<int> di(l);
-
-        long demandvertex = 0;
-        long bugdetector = 0;
-
-        for(int i=0; i < l; i++) {
-            cin >> gi[i] >> di[i];
-            demandvertex += di[i];
-            bugdetector += gi[i];
-            cerr << "gi/di " << gi[i] << " " << di[i] << endl;
-        }
-        cerr << "Demand= " << demandvertex << " / " << bugdetector << endl;
-
-        vector<int> from(p);
-        vector<int> to(p);
-
-        vector<int> min_capacity(p);
-        vector<int> max_capacity(p);
-
-
-        int capacitysum = 0;
-
-        for(int i=0; i < p; i++) {
-            cin >> from[i] >> to[i] >> min_capacity[i] >> max_capacity[i];
-            capacitysum += max_capacity[i];
-        }
-
         Graph G(l+2);
         EdgeCapacityMap capacitymap = get(edge_capacity, G);
         ReverseEdgeMap revedgemap = get(edge_reverse, G);
-        ResidualCapacityMap rescapacitymap = get(edge_residual_capacity, G);
         EdgeAdder eaG(G, capacitymap, revedgemap);
 
-        /* add intra graph edges w/ minimum capacity
-         * s -> v
-         * u -> t
-         * reduced u->v
-         */
 
         int n_source = l;
         int n_target = l+1;
 
-        vector<int> min_s_demand(l,0);
-        vector<int> min_t_demand(l,0);
+        long demandvertex = 0;
+        for(int i=0; i < l; i++) {
+            long gi, di;
 
-        int demandedge = 0;
-
-        for(int i=0; i < p; i++) {
-            int c = max_capacity[i] - min_capacity[i];
-
-            min_s_demand[to[i]]   +=  min_capacity[i];
-            min_t_demand[from[i]] +=  min_capacity[i];
-            demandedge            += min_capacity[i];
-
-            eaG.addEdge(from[i], to[i], c); // u->v
+            cin >> gi >> di;
+            demandvertex += di;
+            eaG.addEdge(n_source, i, gi); // s->u
+            eaG.addEdge(i, n_target, di); // u->t
         }
 
-        for(int i=0; i < l; i++) {
-            eaG.addEdge(n_source, i, gi[i] + min_s_demand[i]); // s->u
-            eaG.addEdge(i, n_target, di[i] + min_t_demand[i]); // u->t
+        long demandedge = 0;
+        for(int i=0; i < p; i++) {
+            long n_from, n_to, min_c, max_c;
+
+            cin >> n_from >> n_to >> min_c >> max_c;
+
+            demandedge += min_c;
+
+            long c = max_c - min_c;
+
+            eaG.addEdge(n_source, n_to, min_c); // s->v
+            eaG.addEdge(n_from, n_target, min_c); // u->t
+            eaG.addEdge(n_from, n_to, c); // u->v
         }
 
         long flow = push_relabel_max_flow(G, n_source, n_target);
-
-        //long flow = boost::edmonds_karp_max_flow(G, n_source, n_target);
 
         if(flow >= (demandvertex+demandedge)) {
             cout << "yes\n";
@@ -141,6 +108,4 @@ int main()
 
 
     }
-    exit(0);
-
 }
