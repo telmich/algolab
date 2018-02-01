f67ef5ffa00ce0d7e1c69b74577e16c965c3f82e
Nico Schottelius <nico@nico-notebook.schottelius.org>
2018-02-01 15:06:18 +0100

    Working on first_hit

--- first_hit.cpp
+++ first_hit.cpp
@@ -1,4 +1,6 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
 #include <CGAL/intersections.h>
 
 #include <boost/format.hpp>
@@ -9,22 +11,26 @@
 using namespace std;
 using namespace CGAL;
 
-// typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K_inexact;
 typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+
 typedef K::Point_2 P;
 typedef K::Line_2 L;
 typedef K::Ray_2 R;
 typedef K::Segment_2 S;
 typedef K::Intersect_2 Intersect_2;
 
-/* coordinates: 2^51 -- no integers */
+typedef K::Point_2 Point;
+typedef K::Ray_2 Ray;
+typedef K::Segment_2 Segment;
+typedef K::FT FT;
 
-inline double distance_ray(bool xaxis, P ray, P p)
-{
-    return xaxis ? to_double((abs(p.x() - ray.x()))) : to_double((abs(p.y() - ray.y())));
-}
+typedef K_inexact::Ray_2 RayE;
+typedef K_inexact::Segment_2 SegmentE;
+typedef K_inexact::Point_2 PointE;
 
-double floor_to_double(const K::FT& x) {
+
+double floor_to_long(const K::FT& x) {
     double a = std::floor(CGAL::to_double(x));
     while (a > x) a -= 1;
     while (a+1 <= x) a += 1;
@@ -39,103 +45,94 @@ int main()
 
     cin >> n;
 
-    S *s_point;
-
     while(n) {
-        double ray[4];
-        double obstacles[4];
+
+        long r,s,t,u;
+
+        cin >> r >> s >> t >> u;
+        Point p1(r,s), p2(t,u);
+        PointE p1E(r,s), p2E(t,u);
+
+        Ray ray = Ray(p1, p2);
+        RayE rayE = RayE(p1E, p2E);
+
         bool intersects = false;
-        bool positive, xaxis;
+        FT distance = -1;
+        Point hit;
+        PointE hitE;
 
-        long distance = LONG_MAX;
-        long ltmp;
-        long p_x = -42, p_y = -42;
-        P p;
+        cout << std::setprecision(20);
 
-        cin >> ray[0] >> ray[1] >> ray[2] >> ray[3];
-
-        P rayx(ray[0], ray[1]), rayy(ray[2], ray[3]);
-
-        R rayline(rayx, rayy);
-
-        /* pre calculate distance function */
-        if(ray[2] > ray[0]) { /* grows in positive direction */
-            positive = true;
-            xaxis = true;
-        } else if (ray[2] < ray[0]) {
-            positive = false;
-            xaxis = true;
-        } else if (ray[3] > ray[1]) {
-            positive = true;
-            xaxis = false;
-        } else {
-            positive = true;
-            xaxis = false;
-        }
-
-        // cerr << "before for\n";
         for(int i=0; i < n; ++i) {
-            cin >> obstacles[0] >> obstacles[1] >> obstacles[2] >> obstacles[3];
+            long a, b, c, d;
 
-            S segment(P(obstacles[0], obstacles[1]), P(obstacles[2], obstacles[3]));
+            cin >> a >> b >> c >> d;
 
-            // cerr << "before do_intersect\n";
-            if(do_intersect(segment, rayline)) {
-                intersects = true;
-                long p_x_tmp, p_y_tmp;
+            Point o1(a,b), o2(c,d);
+            PointE o1E(a,b), o2E(c,d);
 
-                // cerr << "before auto\n";
-                auto o = intersection(segment, rayline);
-
-                if(!o) {
-                    // cerr << "no intersection in intersection\n";
-                }
-                P *i_point;
+            Point thispoint;
+            FT    thisdist;
 
 
-                if(i_point = boost::get<P>(&*o)) {
-                    // cerr << "in P\n";
-                    ltmp = distance_ray(xaxis, rayx, *i_point);
-                    p_x_tmp  = floor(to_double(i_point->x()));
-                    p_y_tmp =  floor(to_double(i_point->y()));
-                    p = *i_point;
+            Segment  obstacle  = Segment(o1, o2);
+            SegmentE obstacleE = SegmentE(o1E, o2E);
 
-                } else if (s_point = boost::get<S>(&*o)) {
-                        ltmp = distance_ray(xaxis, rayx, s_point->source());
-                        p = s_point->source();
-                        p_x_tmp = floor(to_double(s_point->source().x()));
-                        p_y_tmp = floor(to_double(s_point->source().y()));
-                    } else {
-                    cerr << "uncaught branch\n";
-                }
+            bool this_intersects = false;
 
-
-                if(ltmp < distance) {
-                    distance = ltmp;
-                    p_x  = p_x_tmp;
-                    p_y =  p_y_tmp;
-                    // double dtmp = to_double(p.x());
-                    // cerr << "p=" << dtmp << " " << boost::format("%15.5d") % p.x() << " " << p.y() << endl;
-                    // printf("%15.10lf\n", dtmp);
-                    // cerr << "p2=" << p_x_tmp << " " << p_y_tmp << endl;
-                }
+            if(intersects) { /* clipping */
+                SegmentE segE(p1E, hitE);
+                this_intersects = CGAL::do_intersect(segE, obstacleE);
+            } else {
+                this_intersects = CGAL::do_intersect(rayE, obstacleE);
             }
 
+            Segment *sp;
+            Point   *pp;
+
+            if(this_intersects) {
+                auto o = CGAL::intersection(ray, obstacle);
+
+                if(pp = boost::get<Point>(&*o)) {
+                    thispoint = *pp;
+                    thisdist  = CGAL::squared_distance(p1, thispoint);
+                } else if (sp = boost::get<Segment>(&*o)) {
+
+                    Point s1 = sp->source();
+                    Point s2 = sp->target();
+
+                    FT dist1 = CGAL::squared_distance(p1, s1);
+                    FT dist2 = CGAL::squared_distance(p1, s2);
+
+                    if(dist1 > dist2) {
+                        thisdist = dist2;
+                        thispoint = s2;
+                    } else {
+                        thisdist = dist1;
+                        thispoint = s1;
+                    }
+
+                }
+//                cerr << "got intersection at " << thispoint << " " << thisdist << endl;
+
+                if(!intersects) {                 /* first time */
+                    intersects = true;
+                    distance = thisdist;
+                    hit      = thispoint;
+                } else {                          /* update */
+                    if(thisdist < distance) {
+                        distance = thisdist;
+                        hit      = thispoint;
+                    }
+                }
+            }
         }
+
         if(intersects) {
-            // cout << p_x << " " << p_y << "\n";
-            cout << floor_to_double(p.x()) << " " << floor_to_double(p.y()) << "\n";
+            cout << floor_to_long(hit.x()) << " " << floor_to_long(hit.y()) << "\n";
         } else {
             cout << "no\n";
         }
         cin >> n;
-
     }
-
 }
-
-/*
-
--231170456850865.3125000000
-
-*/
